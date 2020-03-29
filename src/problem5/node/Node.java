8b12a920961fac0b85423df7547100032fba0c41
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-03-28 19:49:48 -0700

    made the constructor and the getter and setter for it

--- src/problem5/node/Node.java
+++ src/problem5/node/Node.java
@@ -5,6 +5,27 @@
  *  Time: 7:06 PM
  */
 package problem5.node;
+
 // to define node properties
 public class Node {
+    public Node next = null;
+    int data = 0;
+
+    public Node(Node next, int data) {
+        this.next = next;
+        this.data = data;
+    }
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public int getData() {
+        return data;
+    }
 }
