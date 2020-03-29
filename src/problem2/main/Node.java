1cb9f4e0903515671994d4ff6a7694da0b534252
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-03-28 20:48:58 -0700

    made getter and setter and constructor

--- src/problem2/main/Node.java
+++ src/problem2/main/Node.java
@@ -4,4 +4,16 @@ public class Node {
     Node next = null;
     int data;
 
+    public Node(Node next, int data) {
+        this.next = next;
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public int getData() {
+        return data;
+    }
 }
